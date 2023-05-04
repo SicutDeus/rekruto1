e3616b877580da008a29669da13ababb87db206f
SicutDeus <mich17818@gmail.com>
2023-05-04 16:09:25 +0300

    initial

--- testtask/testtask/settings.py
+++ testtask/testtask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4=0mqlnc439)*jrhmio-1$c)#_lr4#+^*u-h*=(^#mx0q=9q_(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
